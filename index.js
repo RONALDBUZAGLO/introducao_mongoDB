a463bdfabfa000c228b2f6d76e0dd4f66049fc15
ronaldbuzaglo <senseironald@gmail.com>
2021-03-27 10:38:23 -0400

    implementação do delete e edit

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const mongoose = require("mongoose");
 const articleModel = require("./article");
 
-mongoose.connect("mongodb://localhost:27017/aprendendoMongo",{ useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/aprendendoMongo",{ useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false});
 
 const Article = mongoose.model('Article',articleModel);
 
+Article.findByIdAndUpdate("605e7ab2e5c9c2239c7580e1",{body:"mobile programing"}).then(()=>{
+    console.log("Atualizado com sucesso");
+}).catch(err=>{
+    console.log(err);
+});
+
+
+// Article.findByIdAndDelete("605e7878dd25023268ed4c6f").then(()=>{
+//     console.log("dado removido");
+// }).catch(err=>{
+//     console.log(err);
+// });
+
+// Article.findOne({'title': 'reaasdctjs do zero'}).then(article=>{
+//     console.log(article);
+// }).catch(err=>{
+//     console.log(err);
+// });
+
+
+
+
+// Article.find({'_id': '605e7ab2e5c9c2239c7580e1',
+//     'title': 'reactjs do zero'}).then(article=>{
+//     console.log(article);
+// }).catch(err=>{
+//     console.log(err);
+// });
+
+
+
+
+// Article.find({}).then(articles=>{
+//     console.log(articles);
+// }).catch(err=>{
+//     console.log(err);
+// });
+
+
+
+
 // const artigo = new Article({
-//     title:"aslkdjalksjd",
+//     title:"reactjs do zero",
 //     author: "Ronald Buzaglo",
 //     body: "zxmcn laskjd pqwoie",
 //     special:true,
